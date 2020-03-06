021839a08ca5ddeacdac0013b159d0eafb39e51c
Kevin Liu <dethsandwich@gmail.com>
2020-03-06 13:31:32 -0500

    Lab05 A

--- app/src/main/java/com/liukevin/lab05/MainActivity.java
+++ app/src/main/java/com/liukevin/lab05/MainActivity.java
@@ -5,50 +5,103 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    final SharedPreferences sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
-    TextView text = findViewById(R.id.text);
-    Button button = findViewById(R.id.button);
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    TextView text;
+    TextView set1;
+    int settings;
+    TextView set2;
+    Button button;
+    int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
+        sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        text = findViewById(R.id.text);
+        set1 = findViewById(R.id.set1);
+        set2 = findViewById(R.id.set2);
+        button = findViewById(R.id.button);
+        count = 0;
+        editor = sharedPreferences.edit();
+        settings = sharedPreferences.getInt("counter", 0);
+        editor.putInt("counter", settings + 1);
+        set2.setText("Set 2: " + sharedPreferences.getInt("counter", 0));
+        count++;
+        set1.setText("Set 1: " + count);
+        Log.i("onCreate", "onCreate");
         editor.apply();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        settings = sharedPreferences.getInt("counter", 0);
+        editor.putInt("counter", settings + 1);
+        set2.setText("Set 2: " + sharedPreferences.getInt("counter", 0));
+        count++;
+        set1.setText("Set 1: " + count);
+        editor.apply();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        settings = sharedPreferences.getInt("counter", 0);
+        editor.putInt("counter", settings + 1);
+        set2.setText("Set 2: " + sharedPreferences.getInt("counter", 0));
+        count++;
+        set1.setText("Set 1: " + count);
+        editor.apply();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        settings = sharedPreferences.getInt("counter", 0);
+        editor.putInt("counter", settings + 1);
+        set2.setText("Set 2: " + sharedPreferences.getInt("counter", 0));
+        count++;
+        set1.setText("Set 1: " + count);
+        editor.apply();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        settings = sharedPreferences.getInt("counter", 0);
+        editor.putInt("counter", settings + 1);
+        set2.setText("Set 2: " + sharedPreferences.getInt("counter", 0));
+        count++;
+        set1.setText("Set 1: " + count);
+        editor.apply();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        settings = sharedPreferences.getInt("counter", 0);
+        editor.putInt("counter", settings + 1);
+        set2.setText("Set 2: " + sharedPreferences.getInt("counter", 0));
+        count++;
+        set1.setText("Set 1: " + count);
+        editor.apply();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        settings = sharedPreferences.getInt("counter", 0);
+        editor.putInt("counter", settings + 1);
+        set2.setText("Set 2: " + sharedPreferences.getInt("counter", 0));
+        count++;
+        set1.setText("Set 1: " + count);
+        editor.apply();
     }
 }
